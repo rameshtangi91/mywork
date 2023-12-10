3ff36d8d936e4505680595c5de2f46ebf3e6044e
ram <mymail@gmail.com>
2023-12-10 13:28:11 +0530

    commit

--- Basics/src/basicspractice/Helloworld.java
+++ Basics/src/basicspractice/Helloworld.java
@@ -2,6 +2,6 @@ package basicspractice;
 
 public class Helloworld {
  public static void main(String[] args) {
-	 System.out.println("Hai welcome to My Java world");
+	 System.out.println("Hai welcome to My Java world - Java");
  }
 }
