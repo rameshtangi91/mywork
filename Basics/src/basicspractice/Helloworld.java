121ce13b6e26a73a7d7e6a6d82d94470ef58a2b3
ram <mymail@gmail.com>
2023-12-10 13:35:28 +0530

    tets

--- Basics/src/basicspractice/Helloworld.java
+++ Basics/src/basicspractice/Helloworld.java
@@ -2,6 +2,6 @@ package basicspractice;
 
 public class Helloworld {
  public static void main(String[] args) {
-	 System.out.println("Hai welcome to My Java world - Java");
+	 System.out.println("Hai welcome to My Java world - Java tets");
  }
 }
